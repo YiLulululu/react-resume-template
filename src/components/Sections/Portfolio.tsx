2c2c8f3430f2030bf90956ccc7f7576f0e097045
Lu <yiluy@live.unc.edu>
2023-11-15 16:26:22 -0500

    Updated hero section

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import {FC, memo, MouseEvent, useCallback, useEffect, useRef, useState} from 'react';
 
 import {isMobile} from '../../config';
-import {portfolioItems, SectionId} from '../../data/data';
+import {portfolioCovidItems, portfolioPCItems, portfolioPensionItems, SectionId} from '../../data/data';
 import {PortfolioItem} from '../../data/dataDef';
 import useDetectOutsideClick from '../../hooks/useDetectOutsideClick';
 import Section from '../Layout/Section';
@@ -13,9 +13,10 @@ const Portfolio: FC = memo(() => {
   return (
     <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
       <div className="flex flex-col gap-y-8">
-        <h2 className="self-center text-3xl font-bold text-white">Projects</h2>
+      <h2 className="self-center text-3xl font-bold text-white">Projects</h2>
+        <h2 className="self-center text-3xl font-bold text-white">Private Capital</h2>
         <div className=" w-full columns-1 "> {/*md:columns-3 lg:columns-4*/}
-          {portfolioItems.map((item, index) => {
+          {portfolioPCItems.map((item, index) => {
             const {title, image} = item;
             return (
               <div className="pb-6" key={`${title}-${index}`}>
@@ -30,6 +31,43 @@ const Portfolio: FC = memo(() => {
             );
           })}
         </div>
+
+        <h2 className="self-center text-3xl font-bold text-white">Pension</h2>
+        <div className=" w-full columns-1 "> {/*md:columns-3 lg:columns-4*/}
+          {portfolioPensionItems.map((item, index) => {
+            const {title, image} = item;
+            return (
+              <div className="pb-6" key={`${title}-${index}`}>
+                <div
+                  className={classNames(
+                    'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
+                  )}>
+                  <Image alt={title} className="h-full w-full" placeholder="blur" src={image} />
+                  <ItemOverlay item={item} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        <h2 className="self-center text-3xl font-bold text-white">Covid</h2>
+        <div className=" w-full columns-1 "> {/*md:columns-3 lg:columns-4*/}
+          {portfolioCovidItems.map((item, index) => {
+            const {title, image} = item;
+            return (
+              <div className="pb-6" key={`${title}-${index}`}>
+                <div
+                  className={classNames(
+                    'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
+                  )}>
+                  <Image alt={title} className="h-full w-full" placeholder="blur" src={image} />
+                  <ItemOverlay item={item} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
       </div>
     </Section>
   );
